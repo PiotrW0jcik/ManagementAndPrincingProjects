15ce84a23be172ddcd28e4d9ac3f08c3c9b25440
PiotrW0jcik <p.w9714x3@gmail.com>
2020-05-26 22:42:56 +0200

    Add Data Addnotation to Employee model

--- ManagementAndPricingOfProjectsMVC/Models/Employee.cs
+++ ManagementAndPricingOfProjectsMVC/Models/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,13 @@ namespace ManagementAndPricingOfProjectsMVC.Models
     {
         [Key]
         public int EmployeeId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
         public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
         public string Position { get; set; }
+        [Required]
         public int PricePerHour { get; set; }
 
     }
