a5339d53a4cd40ea7fd01e6ce7d1e048f1886c45
PiotrW0jcik <p.w9714x3@gmail.com>
2020-05-26 22:38:01 +0200

    Add Employee model

---
+++ ManagementAndPricingOfProjectsMVC/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementAndPricingOfProjectsMVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public int PricePerHour { get; set; }
+
+    }
+}
