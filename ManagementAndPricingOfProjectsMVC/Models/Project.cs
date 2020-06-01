074fbd356ab2a5fcfbf7a1c5b331f6c50ccc0582
PiotrW0jcik <p.w9714x3@gmail.com>
2020-06-01 23:36:10 +0200

    Add new Models

---
+++ ManagementAndPricingOfProjectsMVC/Models/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+
+namespace ManagementAndPricingOfProjectsMVC.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            Tasks = new HashSet<Task>();
+        }
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public int ProjectId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string ProjectName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+        public int PriceForProject { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ ManagementAndPricingOfProjectsMVC/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementAndPricingOfProjectsMVC.Models
+{
+   
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        public int EmployeeID { get; set; }
+        public int ProjectID { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string TaskName { get; set; }
+
+        [Required]
+        public int Hours { get; set; }
+        [Required]
+        public int PricePerHour { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Project Project { get; set; }
+       
+    }
+}
